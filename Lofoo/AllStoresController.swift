84107a8fcc964e2d30ef7a085b42ca3d85106256
Igor Vorobiov <ivorobioff@igors-macbook-pro.local>
2016-01-20 12:04:07 +0200

    some cool updates

--- Lofoo/AllStoresController.swift
+++ Lofoo/AllStoresController.swift
@@ -15,41 +15,30 @@ class AllStoresController: UITableViewController {
         ["name": "Nr.1", "location": "28 Ion Creanga"]
     ]
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
         
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addStore")
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .Add,
+            target: self,
+            action: "addStore"
+        )
         
         self.navigationItem.rightBarButtonItem = addButton
     }
     
     func addStore(){
-        performSegueWithIdentifier("toCreateStoreScene", sender: self)
-    }
-    
-    @IBAction func cancelAddStore(sender: UIStoryboardSegue){
         
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
-        super.prepareForSegue(segue, sender: sender)
-        
-        if (segue.identifier != "toCreateStoreScene"){
-            return
+        UIView.animateWithDuration(0.2) { () -> Void in
+            self.splitViewController!.preferredDisplayMode = .PrimaryHidden
+            self.splitViewController!.preferredDisplayMode = .Automatic
         }
+        
+        self.performSegueWithIdentifier("toAddStore", sender: self)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
+    
+    @IBAction func cancelAddStore(sender: UIStoryboardSegue){}
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
@@ -70,51 +59,4 @@ class AllStoresController: UITableViewController {
         
         return cell
     }
-    
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
